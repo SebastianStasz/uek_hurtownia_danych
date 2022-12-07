12c81002f01aafde92eb7b21f6eb1c15f42b0d12
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2022-12-07 16:11:16 +0100

    Load and decode world cup data

--- WorldCupDataAnalyzer/WorldCupDataAnalyzer/main.swift
+++ WorldCupDataAnalyzer/WorldCupDataAnalyzer/main.swift
@@ -7,4 +7,17 @@
 
 import Foundation
 
+private func readWorldCupData() -> Data? {
+    let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+    let bundleURL = URL(fileURLWithPath: "data.bundle", relativeTo: currentDirectoryURL)
+    let bundle = Bundle(url: bundleURL)!
+    let bundlePath = bundle.path(forResource: "worldCupData", ofType: "json")!
+    let jsonData = try! String(contentsOfFile: bundlePath).data(using: .utf8)
+    return jsonData
+}
 
+
+let worldCupData = readWorldCupData()!
+let worldCup = try! JSONDecoder().decode(WorldCup.self, from: worldCupData)
+
+print(worldCup)
